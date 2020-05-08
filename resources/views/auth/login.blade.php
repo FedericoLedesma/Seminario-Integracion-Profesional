5da6a32c593f84689fefe12bd76b03c4a18c9feb
Federico Ledesma <federledesma10@gmail.com>
2020-05-08 14:16:20 -0300

    Corrección en la view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,7 +54,7 @@
                         </div> -->
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contrase&ntilde;a') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -82,7 +82,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Iniciar Sesi&oacute;n') }}
+                                    {{ __('Iniciar Sesión') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
