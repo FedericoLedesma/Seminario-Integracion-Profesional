f8b0fc92123cff63eca52d90d5bf808bdf5e95a4
Federico Ledesma <federledesma10@gmail.com>
2020-05-12 01:41:27 -0300

    modelo Persona y modelo TipoDni agregado

---
+++ database/migrations/2020_05_12_011824_create_personas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('dni');
+            $table->string('apellido',50);
+            $table->string('name',50);
+            $table->string('observacion',100);
+            $table->string('direccion',50);
+            $table->string('email');
+            $table->string('provincia',50);
+            $table->string('localidad',50);
+            $table->string('sexo',25);
+            $table->date('fecha_nac');
+            $table->unsignedInteger('tipo_dni_id');
+            $table->foreign('tipo_dni_id')->references('id')->on('tipo_dnis');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
