9bc76d3d7e9b9393c884b48839118960f5d81134
Federico Ledesma <federledesma10@gmail.com>
2020-05-14 17:59:01 -0300

    Metodos en Paciente agregados

--- app/Paciente.php
+++ app/Paciente.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Paciente extends Model
 {
-    //
+  protected $table = "pacientes";
+
+  protected $fillable = [
+      'id',
+  ];
+  public static function findById(int $id)
+  {
+       $sector = static::where('id', $id)->first();
+       if($sector){
+         return $sector;
+     } return null;
+  }
 }
