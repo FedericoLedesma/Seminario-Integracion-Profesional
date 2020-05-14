92830971b920df113ad2a2c58e29d0bd8d214b14
Federico Ledesma <federledesma10@gmail.com>
2020-05-14 17:56:09 -0300

    Modelos Racion y Sector modificados
    Se agregaron metodos para las consultas en los modelos mencionados.

--- app/Racion.php
+++ app/Racion.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Racion extends Model
 {
-    //
+  protected $table = "racions";
+
+  protected $fillable = [
+      'id', 'name', 'observacion',
+  ];
+  public static function findById(int $id)
+  {
+       $racion = static::where('id', $id)->first();
+       if($racion){
+         return $racion;
+     } return null;
+  }
+  public function scopeFindByName($query,$name)
+  {
+    if($name){
+      return $query->where('name','LIKE','%'.$name.'%')->orderBy('id', 'asc');
+    }return null;
+  }
+  
+
 }
--- app/Sector.php
+++ app/Sector.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sector extends Model
 {
-    //
+  protected $table = "sectors";
+
+  protected $fillable = [
+      'id', 'name', 'descripcion',
+  ];
+  public static function findById(int $id)
+  {
+       $sector = static::where('id', $id)->first();
+       if($sector){
+         return $sector;
+     } return null;
+  }
+  public function scopeFindByName($query,$name)
+  {
+    if($name){
+      return $query->where('name','LIKE','%'.$name.'%')->orderBy('id', 'asc');
+    }return null;
+  }
 }
