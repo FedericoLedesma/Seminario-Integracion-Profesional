35664ddd0936de6c582d34a47e8351de4a161896
Federico Ledesma <federledesma10@gmail.com>
2020-05-14 18:05:07 -0300

    Metodos de consulta en Personal

--- app/Personal.php
+++ app/Personal.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Personal extends Model
 {
-    //
+  protected $table = "personals";
+
+  protected $fillable = [
+      'id', 'matricula',
+  ];
+  public static function findById($id)
+  {
+      if($id){
+         $personal = static::where('id', $id)->first();
+         if($personal){
+           return $personal;
+       }
+     }return null;
+  }
+  public static function findByMatricula($matricula)
+  {
+      if($matricula){
+       $personal = static::where('matricula', $matricula)->first();
+       if($personal){
+         return $personal;
+     }
+    }return null;
+  }
 }
