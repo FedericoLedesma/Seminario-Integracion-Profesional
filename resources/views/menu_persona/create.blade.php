a9e6de471e5a8babee2aeb031dc50be389d36c97
NLarena <nicolarena@hotmail.com>
2020-05-18 10:02:41 -0300

    Agregado insertar menu persona

--- resources/views/menu_persona/create.blade.php
+++ resources/views/menu_persona/create.blade.php
@@ -1,3 +1,14 @@
+<head>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
+</head>
+
+
 @extends('layouts.layout')
 @section('navegacion')
     <li class="breadcrumb-item"><a href="{{route('roles.index') }}">Roles</a></li>
@@ -9,47 +20,75 @@
 <!-- validar los campos y establecer el campo contrase�a -->
 <!-- mostrar una tabla con los roles que existen -->
 
-		{!!Form::open(['route'=>'menu_persona.create','method'=>'GET']) !!}
-			<div class="input-group mb-3">
+<h1>Agregar menu persona (planilla)</h1>
 
-			<select class="browser-default custom-select" id="busqueda_por" name="busqueda_por">
+	{!!Form::open(['route'=>'menu_persona.create','method'=>'GET']) !!}
+		<div class="input-group mb-3">
+
+			<input class="date form-control" type="text" id="calendario" name="calendario">
+			<script type="text/javascript" id="calendario_" name="calendario_"> 
+				var new_date = $('.date').datepicker({format: 'yyyy-mm-dd'});  
+			</script>  
+
+			<select class="browser-default custom-select" id="horario" name="horario">
 				@foreach($horarios as $horario)
 				<option value= {{$horario->id}} >{{$horario->name}}</option>	
 				@endforeach		
 			</select>
-
-				{!!	Form::text('roleid',null,['id'=>'roleid','class'=>'form-control','name'=>'search','placeholder'=>'Ingrese el texto'])!!}
-				<div class="input-group-append">
-				{!!	Form::submit('Buscar',['class'=>'btn btn-success btn-buscar'])!!}
-				</div>
+		<div class="input-group-append">
+		{!!	Form::submit('Ir a dia/horario',['class'=>'btn btn-success btn-buscar'])!!}
+	{!! Form::close() !!}
+	{!!Form::open(['route'=>'menu_persona.store','method'=>'GET']) !!}
+		</div>
 			</div>
-		{!! Form::close() !!}
-
-	    {!!Form::open(['method'=>'post','action'=>'MenuPersonaController@store'])!!}
-	    <h1>Agregar menu persona (planilla)</h1>
-	      @include('layouts.error')
-	    <table>
-	    	<tr>
-	    	<td>
-		    {!!	Form::label('name', 'Nombre de la persona')!!}
-		    </td>
-		    <td>
-			<select class="browser-default custom-select" id="busqueda_por" name="busqueda_por">
-						 <option value="busqueda_nombre_persona" >Buscar por persona</option>
-						 <option value="busqueda_nombre_horario" >Buscar por horario</option>
-						 <option value="busqueda_fecha" >Buscar por fecha</option>
-					 </select>
-		   	</td>
-		   	</tr>
-		   	<tr>
-	    	<td>
-		    {!!	Form::submit('Crear Rol',['class' => 'btn btn-success'])!!}
-		    </td>
-		    <td>
-		   	{!!	Form::reset('Borrar',['class' => 'btn btn-secondary'])!!}
-		   	</td>
-		   	</tr>
-		 </table>
+			@include('layouts.error')
+			<table>
+				<tr>
+					<td>
+					{!!	Form::label('name', 'Nombre de la persona')!!}
+					<select class="browser-default custom-select" id="persona" name="persona">
+						<option value='0'> ninguno </option>
+						@foreach($pacientes as $paciente)
+							<option value= {{$paciente->id}} >{{$paciente->name}} {{$paciente->apellido}}</option>
+						@endforeach
+					</select>
+					</td>					
+					<td>
+					{!!	Form::label('name', 'Racion')!!}
+					<select class="browser-default custom-select" id="racion" name="racion">
+						<option value='0'> ninguna </option>
+						@foreach($raciones_disponibles as $racion)
+							{{Log::debug('Dando vueltas. Racion: '.$racion)}}
+							<option value= {{$racion->id}} > {{$racion->name}}</option>
+						@endforeach
+					</select>
+					</td>
+					<td>
+					{!!	Form::label('name', 'Fecha')!!}
+					<select class="browser-default custom-select" id="fecha" name="fecha">
+						@if($fecha)
+						<option value=$fecha> {{$fecha}} </option>
+						@endif
+					</select>
+					</td>
+					<td>
+					{!!	Form::label('name', 'Horario')!!}
+					<select class="browser-default custom-select" id="horario" name="horario">
+						@if($horario)
+						<option value= {{$horario->id}}> {{$horario->name}} </option>
+						@endif
+					</select>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						{!!	Form::submit('Cargar menu persona (planilla)',['class' => 'btn btn-success'])!!}
+					</td>
+					<td>
+						{!!	Form::reset('Borrar',['class' => 'btn btn-secondary'])!!}
+					</td>
+				</tr>
+			</table>
 		{!! Form::close() !!}
 
 @endsection
