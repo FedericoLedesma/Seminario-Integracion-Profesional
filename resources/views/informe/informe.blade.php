42088309ff9c3bc4fd56f9b9ada103e395ba678b
Federico Ledesma <federledesma10@gmail.com>
2020-06-05 13:05:59 -0300

    :Vista de informes modificada

--- resources/views/informe/informe.blade.php
+++ resources/views/informe/informe.blade.php
@@ -3,9 +3,9 @@
 <h1>Informe de Menus</h1>
 
     <div>
-      <h2>Creado : {{$creado}} </h2>
-      <p>Solicitado por el usuario : Apellido: {{$user->personal->persona->apellido}} Nombre: {{$user->personal->persona->name}} </p>
-      <p>Numero de documento {{$user->personal->persona->numero_doc}} ID: {{$user->personal->persona->id}}</p>
+      <h2>Solicitado : {{$creado}} </h2>
+      <p>Por el usuario : Apellido: {{$user->personal->persona->apellido}} Nombre: {{$user->personal->persona->name}} </p>
+      <p>Numero de documento {{$user->personal->persona->numero_doc}} - ID: {{$user->personal->persona->id}}</p>
       <p>
         Menus por : {{$busqueda_por}}  {{$query}} <br>
         Cantidad encontrada= {{$menus_total}}
@@ -21,7 +21,7 @@
               <th scope="col">Horario</th>
               <th scope="col">Racion</th>
               <th scope="col">Fecha</th>
-              <th scope="col">Realizado</th>              
+              <th scope="col">Entregado</th>
 
             </tr>
           </thead>
@@ -32,9 +32,18 @@
             <tr>
               {{Log::debug(' Persona id: '.$menu_persona)}}
               <td>{{$menu_persona->get_persona_nombre_completo()}}</td>
-              <td>{{$menu_persona->persona->sectorFecha($menu_persona->racionDisponible->fecha)->name}}</td>
-              <td>{{$menu_persona->persona->habitacionFecha($menu_persona->racionDisponible->fecha)->name}}</td>
-              <td>{{$menu_persona->persona->camaFecha($menu_persona->racionDisponible->fecha)->id}}</td>
+              @if($menu_persona->persona->sectorFecha($menu_persona->racionDisponible->fecha))
+                <td>{{$menu_persona->persona->sectorFecha($menu_persona->racionDisponible->fecha)->name}}</td>
+              @else
+                <td>-</td>
+              @endif
+              @if($menu_persona->persona->camaFecha($menu_persona->racionDisponible->fecha))
+                <td>{{$menu_persona->persona->habitacionFecha($menu_persona->racionDisponible->fecha)->name}}</td>
+                <td>{{$menu_persona->persona->camaFecha($menu_persona->racionDisponible->fecha)->id}}</td>
+              @else
+                <td>-</td>
+                <td>-</td>
+              @endif
               <td>{{$menu_persona->get_horario_name()}}</td>
               <td>{{$menu_persona->get_racion_name()}}</td>
               <td>{{$menu_persona->racionDisponible->fecha}}</td>
