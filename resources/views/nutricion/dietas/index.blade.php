a5b4c7a2901fd5feea7bbaa6e5811af0197f070b
Federico Ledesma <federledesma10@gmail.com>
2020-06-11 02:16:41 -0300

    :Vista de dietas agregada

--- resources/views/nutricion/dietas/index.blade.php
+++ resources/views/nutricion/dietas/index.blade.php
@@ -1,35 +1,88 @@
 @extends('layouts.layout')
 @section('token')
 	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<style>
+		div.blueTable {
+		border: 1px solid #1C6EA4;
+		background-color: #EEEEEE;
+		width: 100%;
+		text-align: left;
+		border-collapse: collapse;
+		}
+		.divTable.blueTable .divTableCell, .divTable.blueTable .divTableHead {
+		border: 1px solid #AAAAAA;
+		padding: 3px 2px;
+		}
+		.divTable.blueTable .divTableBody .divTableCell {
+		font-size: 13px;
+		}
+		.divTable.blueTable .divTableRow:nth-child(even) {
+		background: #CBCFD5;
+		}
+		.divTable.blueTable .divTableHeading {
+		background: #1C6EA4;
+		background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+		background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+		background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+		border-bottom: 2px solid #444444;
+		}
+		.divTable.blueTable .divTableHeading .divTableHead {
+		font-size: 15px;
+		font-weight: bold;
+		color: #FFFFFF;
+		border-left: 2px solid #D0E4F5;
+		}
+		.divTable.blueTable .divTableHeading .divTableHead:first-child {
+		border-left: none;
+		}
+
+		.blueTable .tableFootStyle {
+		font-size: 14px;
+		font-weight: bold;
+		color: #FFFFFF;
+		background: #D0E4F5;
+		background: -moz-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+		background: -webkit-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+		background: linear-gradient(to bottom, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+		border-top: 2px solid #444444;
+		}
+		.blueTable .tableFootStyle {
+		font-size: 14px;
+		}
+		.blueTable .tableFootStyle .links {
+		text-align: right;
+		}
+		.blueTable .tableFootStyle .links a{
+		display: inline-block;
+		background: #1C6EA4;
+		color: #FFFFFF;
+		padding: 2px 8px;
+		border-radius: 5px;
+		}
+		.blueTable.outerTableFooter {
+		float:left;
+		}
+		.divTable{ display: table; }
+		.divTableRow { display: table-row; }
+		.divTableHeading { display: table-header-group;}
+		.divTableCell, .divTableHead { display: table-cell;}
+		.divTableHeading { display: table-header-group;}
+		.divTableFoot { display: table-footer-group;}
+		.divTableBody { display: table-row-group;}
+	</style>
 @endsection
 @section('navegacion')
 	<li class="breadcrumb-item active">Dietas</li>
 @endsection
 @section('content')
 
-<!-- INDEX DEL ROL -->
-<!-- validar los campos y establecer el campo contrase�a -->
-<!-- mostrar una tabla con los roles que existen -->
-	    <h1>Alimentos registrados</h1>
-	      @include('layouts.error')
-
-<!-- UTILIZAR PLANTILLA BLADE PARA PERSONALIZAR LAS TABLAS SE REPITE CON ROLES -->
-
-		<style>
-<!--
-.table{
-	 background-color: #E3EEE9;
-
-
-}
--->
-</style>
+	<h1>Dietas registrados</h1>
+	 @include('layouts.error')
 
 <div>
 	<p>
 		<span id="alimentos-total">
 			<!-- Aca deben ir el total de Alimentos -->
-
 		</span>
 	</p>
 	@if($query)
@@ -37,50 +90,53 @@
 	@endif
 </div>
 
+{!!Form::open(['route'=>'dietas.index','method'=>'GET']) !!}
 <div class="container">
-    <div class="table-responsive">
-         <div class="col-md-8 col-md-offset-2">
-					 <div class="panel-heading">
-							 {!!Form::open(['route'=>'dietas.index','method'=>'GET']) !!}
-								 <div class="input-group mb-3">
-
-									 <select class="browser-default custom-select" id="busqueda_por" name="busqueda_por">
-										 <option value="busqueda_id" >ID</option>
-										 <option value="busqueda_patologia" >Id Patologia</option>
-									 </select>
-
-									 {!!	Form::text('dietaid',null,['id'=>'dietaid','class'=>'form-control','name'=>'search','placeholder'=>'Ingrese el texto'])!!}
-									 <div class="input-group-append">
-									{!!	Form::submit('Buscar',['class'=>'btn btn-success btn-buscar'])!!}
-									 </div>
-								 </div>
-								{!! Form::close() !!}
-
-								<table class="table table-striped table-hover "><!--  align="center" border="2" cellpadding="2" cellspacing="2" style="width: 900px;">-->
-									<thead >
-										<tr>
-											<th scope="col">id</th>
-											<th scope="col">Patologia</th>
-											<th scope="col"></th>
-
-										</tr>
-									</thead>
-
-									<tbody>
-									@if($dietas)
-										@foreach($dietas as $dieta)
-										<tr>
-											<td>{{$dieta->id}}</td>
-											<td>{{$dieta->patologia->name}}</td>
-
-
-										</tr>
-											@endforeach
-										@endif
-
-									</table>
-							</div>
+  <div class="table-responsive">
+		<div class="col-md-10 col-md-offset-1">
+			{!!Form::open(['route'=>'raciones.index','method'=>'GET']) !!}
+				<div class="input-group mb-3">
+					<select class="browser-default custom-select" id="busqueda_por" name="busqueda_por">
+						<option value="busqueda_id" >ID</option>
+						<option value="busqueda_name" >Nombre</option>
+					</select>
+					{!!	Form::text('racionid',null,['id'=>'racionid','class'=>'form-control','name'=>'search','placeholder'=>'Ingrese el texto'])!!}
+					<div class="input-group-append">
+					 {!!	Form::submit('Buscar',['class'=>'btn btn-success btn-buscar'])!!}
 					</div>
-			</div>
+				</div>
+			 {!! Form::close() !!}
+		</div>
+	</div>
+</div>
+<div class="col-md-10 col-md-offset-2">
+	<div class="table-responsive">
+		<!--<div class="table table-striped table-hover ">  align="center" border="2" cellpadding="2" cellspacing="2" style="width: 900px;">-->
+		<div class="divTable blueTable">
+			<div class="divTableHeading">
+				<div class="divTableRow">
+						<div class="divTableHead">ID</div>
+						<div class="divTableHead">Observación</div>
+						<div class="divTableHead">Patología</div>
+						<div class="divTableHead">Accion</div>
+					</div>
+				</div>
+
+				@if($dietas)
+					@foreach($dietas as $dieta)
+				<div class="divTableBody">
+					<div class="divTableRow">
+							<div class="divTableCell">{{$dieta->id}}</div>
+							<div class="divTableCell">{{$dieta->observacion}}</div>
+							<div class="divTableCell">{{$dieta->patologia->name}}</div>
+
+							<div class="divTableCell">{!!link_to_route('dietas.show', $title = 'VER', $parameters = [$dieta],['class' => 'btn btn-info'], $attributes = [])!!}
+						</a><button type="submit" class="btn btn-danger eliminar" data-token="{{ csrf_token() }}" data-id="{{ $dieta }}">Eliminar</button>
+						</div>
+					</div>
+				</div>
+			@endforeach
+		@endif
+	</div>
 </div>
 @endsection
---
+++ resources/views/nutricion/dietas/show.blade.php
@@ -0,0 +1,164 @@
+@extends('layouts.layout')
+@section('token')
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+  <style>
+    div.blueTable {
+    border: 1px solid #1C6EA4;
+    background-color: #EEEEEE;
+    width: 100%;
+    text-align: left;
+    border-collapse: collapse;
+  }
+.divTable.blueTable .divTableCell, .divTable.blueTable .divTableHead {
+    border: 1px solid #AAAAAA;
+    padding: 3px 2px;
+  }
+.divTable.blueTable .divTableBody .divTableCell {
+    font-size: 13px;
+  }
+.divTable.blueTable .divTableRow:nth-child(even) {
+    background: #CBCFD5;
+  }
+  .divTable.blueTable .divTableHeading {
+    background: #1C6EA4;
+    background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+    background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+    background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+    border-bottom: 2px solid #444444;
+  }
+  .divTable.blueTable .divTableHeading .divTableHead {
+    font-size: 15px;
+    font-weight: bold;
+    color: #FFFFFF;
+    border-left: 2px solid #D0E4F5;
+  }
+  .divTable.blueTable .divTableHeading .divTableHead:first-child {
+    border-left: none;
+  }
+
+  .blueTable .tableFootStyle {
+    font-size: 14px;
+    font-weight: bold;
+    color: #FFFFFF;
+    background: #D0E4F5;
+    background: -moz-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+    background: -webkit-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+    background: linear-gradient(to bottom, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+    border-top: 2px solid #444444;
+  }
+  .blueTable .tableFootStyle {
+    font-size: 14px;
+  }
+  .blueTable .tableFootStyle .links {
+    text-align: right;
+  }
+  .blueTable .tableFootStyle .links a{
+    display: inline-block;
+    background: #1C6EA4;
+    color: #FFFFFF;
+    padding: 2px 8px;
+    border-radius: 5px;
+  }
+  .blueTable.outerTableFooter {
+		float:left;
+	}
+  .divTable{ display: table; }
+  .divTableRow { display: table-row; }
+  .divTableHeading { display: table-header-group;}
+  .divTableCell, .divTableHead { display: table-cell;}
+  .divTableHeading { display: table-header-group;}
+  .divTableFoot { display: table-footer-group;}
+  .divTableBody { display: table-row-group;}
+</style>
+@endsection
+@section('navegacion')
+    <li class="breadcrumb-item"><a href="{{route('dietas.index') }}">Dietas</a></li>
+		<li class="breadcrumb-item active">Ver dieta</li>
+@endsection
+@section('content')
+
+	   @include('layouts.error')
+	  	@if($dieta)
+
+	    <h2>Dieta:  {{$dieta->id}}</h2>
+      <div class="col-md-5 col-md-offset-1">
+      	<div class="panel-heading">
+         	<div class="divTable blueTable">
+
+    	    	<div class="divTableRow">
+    	    		<div class="divTableCell">ID </div>
+      				<div class="divTableCell">{{$dieta->id}}</div>
+            </div>
+            <div class="divTableRow">
+      				<div class="divTableCell">Observacion </div>
+      				<div class="divTableCell">{{$dieta->observacion}}</div>
+            </div>
+            <div class="divTableRow">
+      				<div class="divTableCell">Patología </div>
+      				<div class="divTableCell">{{$dieta->patologia->name}}</div>
+            </div>
+          </div>
+        </div>
+		</div>
+		<h3>Dietas Activas asociadas</h2>
+		<div class="col-md-5 col-md-offset-1">
+			<div class="panel-heading">
+
+				<div class="divTable blueTable">
+					<div class="divTableHeading">
+						<div class="divTableHead">ID</div>
+						<div class="divTableHead">Fecha desde</div>
+						<div class="divTableHead">Fecha final</div>
+					</div>
+					<div class="divTableBody">
+						@foreach($dieta->dietaActiva as $d_a)
+						<div class="divTableRow">
+							<div class="divTableCell">{{$d_a->id}}</div>
+							<div class="divTableCell">{{$d_a->fecha}}</div>
+							<div class="divTableCell">{{$d_a->fecha_final}}</div>
+							<div class="divTableCell">
+								<a href="#" class="btn btn-success pull-right" data-toggle="modal" data-target="#modal-{{$d_a->id}}">
+									Raciones asociadas
+								</a>
+							</div>
+						</div>
+						@if($d_a->raciones)
+						<div class="modal fade" id="modal-{{$d_a->id}}">
+							<div class="modal-dialog modal-lg">
+								<div class="modal-content">
+									<div class="modal-header" id="modal-header">
+										<h4>Raciones asociadas a la dieta activa  de {{$d_a->id}} </h4>
+										<button type="button" class="close" data-dismiss="modal">
+											<span>×</span>
+										</button>
+									</div>
+									<div class="modal-movimiento-body">
+										<div id="p_body">
+											<table class="table table-striped table-hover ">
+												<thead >
+													<tr>
+														<th scope="col">Racion</th>
+													</tr>
+												</thead>
+												<tbody>
+												@foreach($d_a->raciones as $racion)
+													<tr>
+														<td>{{$racion->name}}</td>
+													</tr>
+												@endforeach
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						@endif
+						@endforeach
+				</div>
+			</div>
+		</div>
+	</div>
+@endif
+
+@endsection
